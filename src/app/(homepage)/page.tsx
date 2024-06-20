2db8bf33b1c34e6931c0564d05ad1f7b8ca02a7e
Andrew Oplas <andrewmoplas@gmail.com>
2024-06-20 14:20:43 +0800

    fix overflow

--- src/app/(homepage)/page.tsx
+++ src/app/(homepage)/page.tsx
@@ -4,7 +4,7 @@ import { FeaturesSection } from "./components/features-section";
 
 export default function Home() {
   return (
-    <main className="min-h-screen">
+    <main className="min-h-screen overflow-x-hidden">
       <div className="bg-[#1E1E1E]">
         <TransportationSection />
         <FeaturesSection />
