99ef96297a098baf422fc8f2b81b428f76167977
Andrew Oplas <andrewmoplas@gmail.com>
2024-06-20 22:23:59 +0800

    update video size of hero section and remove car image

--- src/app/(homepage)/components/hero-section.tsx
+++ src/app/(homepage)/components/hero-section.tsx
@@ -1,40 +1,11 @@
-import Image from "next/image";
-import React from "react";
-import imgHero from "/public/images/mercedes-cars.png";
-
 export const HeroSection = () => (
-  <section className="relative overflow-hidden bg-[#A3A7AD] pb-0 pt-16 md:py-24">
-    <div className="px-default relative z-[1] mx-auto flex min-h-[40dvh] max-w-section">
-      <div className="flex flex-col items-center gap-20 md:flex-row md:gap-12 w-full">
-        <div className="flex shrink-0 flex-col items-center md:items-start">
-          <h1
-            className="flex flex-col gap-1 text-center text-3xl font-normal text-white md:gap-4 md:text-left"
-            style={{
-              textShadow: "3px -1px 5px rgba(0, 0, 0, 0.5);",
-            }}
-          >
-            Velkommen til Nordic
-            <span className="text-6xl text-app-orange">Xpress</span>
-            Limousine Service
-          </h1>
-
-          <button
-            aria-label="book now"
-            className="shadow-[0px 4px 8px 0px rgba(0, 0, 0, 0.15)] mt-8 rounded-full bg-app-orange px-5 py-3 text-sm font-semibold leading-7 text-white transition-all hover:saturate-150 xl:px-7"
-          >
-            BOOK NOW
-          </button>
-        </div>
-
-        <Image src={imgHero} alt="photo of mercedes cars" className="w-full" />
-      </div>
-    </div>
-
-    <div className="absolute left-0 top-0 hidden h-full w-full overflow-hidden md:block">
+  <section className="relative flex flex-col items-center justify-center overflow-hidden bg-[#A3A7AD]">
+    <div className="w-full">
       <div className="absolute left-0 top-0 h-full w-full bg-gradient-to-bl from-zinc-500 to-zinc-800 opacity-50"></div>
 
       <video
-        className="min-h-full w-auto min-w-full max-w-none"
+        // className="min-h-full w-auto min-w-full max-w-none"
+        className="w-full"
         loop
         autoPlay
         muted
@@ -45,6 +16,26 @@ export const HeroSection = () => (
         <source src="videos/hero-background.webm" type="video/webm" />
         Your browser does not support the video tag.
       </video>
+    </div>
+
+    <div className="px-default absolute z-10 flex w-full max-w-section justify-center md:justify-start lg:-mt-[10%] xl:-mt-[20%]">
+      <div className="flex flex-col items-center md:items-start">
+        <h1
+          className="flex flex-col gap-1 text-center text-2xl md:text-3xl font-normal text-white md:gap-4 md:text-left"
+          style={{ textShadow: "3px -1px 5px rgba(0, 0, 0, 0.5);" }}
+        >
+          Velkommen til Nordic
+          <span className="text-4xl md:text-6xl text-app-orange">Xpress</span>
+          Limousine Service
+        </h1>
+
+        <button
+          aria-label="book now"
+          className="shadow-[0px 4px 8px 0px rgba(0, 0, 0, 0.15)] mt-8 rounded-full bg-app-orange px-6 md:px-7 py-2 md:py-3 text-sm font-semibold leading-7 text-white transition-all hover:saturate-150 xl:px-7"
+        >
+          BOOK NOW
+        </button>
+      </div>
     </div>
   </section>
 );
