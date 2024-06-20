6c9a90a8b00b3d448ab03fb57f0a9612f176e033
Andrew Oplas <andrewmoplas@gmail.com>
2024-06-20 16:51:05 +0800

    add sections

--- src/app/(homepage)/page.tsx
+++ src/app/(homepage)/page.tsx
@@ -1,14 +1,16 @@
 import Image from "next/image";
 import { TransportationSection } from "./components/transportation-section";
 import { FeaturesSection } from "./components/features-section";
+import { HeroSection } from "./components/hero-section";
+import { CtaSection } from "./components/cta-section";
 
 export default function Home() {
   return (
-    <main className="min-h-screen overflow-x-hidden">
-      <div className="bg-[#1E1E1E]">
-        <TransportationSection />
-        <FeaturesSection />
-      </div>
+    <main className="min-h-screen overflow-x-hidden bg-[#1E1E1E]">
+      <HeroSection />
+      <TransportationSection />
+      <FeaturesSection />
+      <CtaSection />
     </main>
   );
 }
