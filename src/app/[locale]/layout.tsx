1c1cb2109fc798a1cfc6fde22a4a6a8679694f2b
Andrew Oplas <andrewmoplas@gmail.com>
2024-06-23 23:14:03 +0800

    update main meta tag

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -3,48 +3,59 @@ import { Header } from "@/components/header";
 import { locales } from "@/lib/i18n/pathnames";
 import type { Metadata, Viewport } from "next";
 import { NextIntlClientProvider } from "next-intl";
-import { getMessages, unstable_setRequestLocale } from "next-intl/server";
+import {
+  getMessages,
+  getTranslations,
+  unstable_setRequestLocale,
+} from "next-intl/server";
 import { Source_Sans_3 } from "next/font/google";
 import "../globals.css";
+import { getSiteName } from "@/lib/helper";
 
 const font = Source_Sans_3({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Nordic Xpress Limousine Service",
-  description:
-    "Affordable transportation that doesn't compromise on quality. Enjoy reliable and cost-effective services today",
-  openGraph: {
-    images: "/opengraph-image.jpg",
-  },
-  icons: [
-    {
-      rel: "apple-touch-icon",
-      url: "/apple-touch-icon.png",
-    },
-    {
-      rel: "icon",
-      type: "image/png",
-      sizes: "32x32",
-      url: "/favicon-32x32.png",
-    },
-    {
-      rel: "icon",
-      type: "image/png",
-      sizes: "16x16",
-      url: "/favicon-16x16.png",
-    },
-    {
-      rel: "icon",
-      url: "/favicon.ico",
-    },
-  ],
-};
 
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
   themeColor: "#ffffff",
 };
+
+export async function generateMetadata(props: { params: { locale: string } }) {
+  const t = await getTranslations({
+    locale: props.params.locale,
+    namespace: "general",
+  });
+
+  return {
+    title: getSiteName(),
+    description: t("meta_description"),
+    openGraph: {
+      images: "/opengraph-image.jpg",
+    },
+    icons: [
+      {
+        rel: "apple-touch-icon",
+        url: "/apple-touch-icon.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        url: "/favicon-32x32.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        url: "/favicon-16x16.png",
+      },
+      {
+        rel: "icon",
+        url: "/favicon.ico",
+      },
+    ],
+  };
+}
 
 export function generateStaticParams() {
   return locales.map((locale) => ({ locale }));
