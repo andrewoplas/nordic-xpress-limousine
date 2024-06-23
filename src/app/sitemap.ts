929e3113211888a520cbf1b4cf65dd72d5939a56
Andrew Oplas <andrewmoplas@gmail.com>
2024-06-23 22:42:06 +0800

    update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,14 +1,34 @@
 import { getBaseUrl } from "@/lib/helper";
+import { locales, pathnames } from "@/lib/i18n/pathnames";
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: `${getBaseUrl()}/`,
+  return Object.keys(pathnames).map((key) => {
+    if (key === "/") {
+      return {
+        url: `${getBaseUrl()}/`,
+        lastModified: new Date(),
+        changeFrequency: "monthly",
+        priority: 1,
+      };
+    }
+
+    // @ts-ignore
+    const pathname = pathnames[key];
+    const alternates: any = {};
+
+    locales.forEach((locale) => {
+      alternates[locale] = `${getBaseUrl()}/${locale}/${pathname}/`;
+    });
+
+    return {
+      url: `${getBaseUrl()}/${key}/`,
       lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 0.7,
-    },
-    // Add more URLs here
-  ];
+      changeFrequency: "monthly",
+      priority: 0.9,
+      alternates: {
+        languages: alternates,
+      },
+    };
+  });
 }
