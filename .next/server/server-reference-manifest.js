64850263539717b1e9f043dc23b335b7eb123293
Andrew Oplas <andrewmoplas@gmail.com>
2024-07-15 20:59:24 +0800

    rebuild app

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"IUSbxmUHdfT3TJPdBNxpje6P+9O0EoKj0dzy+nwjRSc=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"vQFGeGoqzQfwLw1A/9V2Wcy33sjXW2GCUTKVvnDAhHU=\"}"
